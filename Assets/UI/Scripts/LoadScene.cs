9f4006eaee71180fc8eb0cd729536ab518233bb4
echo-xiao9 <819601193@qq.com>
2022-05-12 10:18:10 +0800

    add Assets

---
+++ Assets/UI/Scripts/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName;
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(loadScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/book1_book2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class book1_book2 : MonoBehaviour
+{
+    
+
+
+    private Animation animati;
+    public Transform book_1;
+    public Transform book_2;
+    public Transform other;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+
+    bool flag1 = false;
+    bool flag2 = false;
+    void Update()
+    {
+        if (other)
+        {
+            float dist = Vector3.Distance(other.position, transform.position);
+
+            if (dist < 1 && !flag2)
+            {
+                flag1 = true;
+            }
+            if (dist > 1)
+            {
+                flag2 = false;
+                flag1 = false;
+                show_end();
+            }
+
+            if (flag1)
+            {
+                flag1 = false;
+                flag2 = true;
+                show();
+            }
+        }
+    }
+
+    void show()
+    {
+        Debug.Log("start");
+    }
+
+    void show_end()
+    {
+        Debug.Log("end");
+    }
+
+   
+    
+}
